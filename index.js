c7761e783c27fc751eea220ec067f0d32a637b0c
JoaquinDunger <dunger.joaquin9@gmail.com>
2022-11-03 20:46:03 -0300

    agrego servidor en express

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const Manager = require('./manager.js')
-const manager = new Manager()
+// const Manager = require('./manager.js')
+// const manager = new Manager()
 
-let product = {
-    title: "Samsung Galaxy S22 Ultra",
-    price: 140000,
-    thumbnail: "https://tienda.personal.com.ar/images/SG_S22_ultra_Negro_min_234476066d.png"
-}
+// let product = {
+//     title: "Samsung Galaxy S22 Ultra",
+//     price: 140000,
+//     thumbnail: "https://tienda.personal.com.ar/images/SG_S22_ultra_Negro_min_234476066d.png"
+// }
 
 // manager.createProduct(product).then(result => console.log(result))
 // manager.getAll().then(result => console.log(result))
 // manager.getById(1).then(result => console.log(result))
-manager.deleteById(2).then(result => console.log(result))
+// manager.deleteById(2).then(result => console.log(result))
--- manager.js
+++ manager.js
@@ -1,63 +1,63 @@
-const fs = require('fs')
-const pathToFile = './products.json'
+// const fs = require('fs')
+// const pathToFile = './products.json'
 
-class Manager {
-    createProduct = async (product) => {
-        if (!product.title || !product.price || !product.thumbnail) return {status: "error", message: "missing fields"}
-        try {
-            if (fs.existsSync(pathToFile)){
-                let data = await fs.promises.readFile(pathToFile, 'utf-8')
-                let products = JSON.parse(data)
-                let id = products[products.length-1].id+1 
-                product.id = id
-                products.push(product)
-                await fs.promises.writeFile(pathToFile, JSON.stringify(products, null, 2))
-                return {status: "success", message: "product created"}
-            } else {
-                product.id = 1
-                await fs.promises.writeFile(pathToFile, JSON.stringify([product], null, 2))
-                return {status: "success", message:"product created"}
-            }
-        } catch(err) {
-        return {status: "error", message: err.message}
-        }
-    }
+// class Manager {
+//     createProduct = async (product) => {
+//         if (!product.title || !product.price || !product.thumbnail) return {status: "error", message: "missing fields"}
+//         try {
+//             if (fs.existsSync(pathToFile)){
+//                 let data = await fs.promises.readFile(pathToFile, 'utf-8')
+//                 let products = JSON.parse(data)
+//                 let id = products[products.length-1].id+1 
+//                 product.id = id
+//                 products.push(product)
+//                 await fs.promises.writeFile(pathToFile, JSON.stringify(products, null, 2))
+//                 return {status: "success", message: "product created"}
+//             } else {
+//                 product.id = 1
+//                 await fs.promises.writeFile(pathToFile, JSON.stringify([product], null, 2))
+//                 return {status: "success", message:"product created"}
+//             }
+//         } catch(err) {
+//         return {status: "error", message: err.message}
+//         }
+//     } 
 
-        getAll = async () => {
-            if (fs.existsSync(pathToFile)) {
-            let data = await fs.promises.readFile(pathToFile, 'utf-8')
-            let products = JSON.parse(data)
-            return {status: "success", message: products}
-        } else {
-            return {status: "error", message: err.message}
-        }
-    }
+//         getAll = async () => {
+//             if (fs.existsSync(pathToFile)) {
+//             let data = await fs.promises.readFile(pathToFile, 'utf-8')
+//             let products = JSON.parse(data)
+//             return {status: "success", message: products}
+//         } else {
+//             return {status: "error", message: err.message}
+//         }
+//     }
 
-        getById = async (id) => {
-            if (!id) return {status: "error", message: "id required"}
-            if (fs.existsSync(pathToFile)) {
-                let data = await fs.promises.readFile(pathToFile, 'utf-8')
-                let products = JSON.parse(data)
-                let product = products.find(product => product.id === id)
-                if (product) return {status: "success", message: product}
-                return {status: "error", message: "product not found"}
-        } else {
-            return {status: "error", message: err.message}
-        }
-    }
+//         getById = async (id) => {
+//             if (!id) return {status: "error", message: "id required"}
+//             if (fs.existsSync(pathToFile)) {
+//                 let data = await fs.promises.readFile(pathToFile, 'utf-8')
+//                 let products = JSON.parse(data)
+//                 let product = products.find(product => product.id === id)
+//                 if (product) return {status: "success", message: product}
+//                 return {status: "error", message: "product not found"}
+//         } else {
+//             return {status: "error", message: err.message}
+//         }
+//     }
 
-        deleteById = async (id) => {
-            if (!id) return {status: "error", message: "id required"}
-            if (fs.existsSync(pathToFile)) {
-                let data = await fs.promises.readFile(pathToFile, 'utf-8')
-                let products = JSON.parse(data)
-                let newProducts = products.filter(product => product.id !== id)
-                await fs.promises.writeFile(pathToFile, JSON.stringify(newProducts, null, 2))
-                return {status: "success", message:"user deleted"}
-        } else {
-            return {status: "error", message: err.message}
-        }
-        }
-}
+//         deleteById = async (id) => {
+//             if (!id) return {status: "error", message: "id required"}
+//             if (fs.existsSync(pathToFile)) {
+//                 let data = await fs.promises.readFile(pathToFile, 'utf-8')
+//                 let products = JSON.parse(data)
+//                 let newProducts = products.filter(product => product.id !== id)
+//                 await fs.promises.writeFile(pathToFile, JSON.stringify(newProducts, null, 2))
+//                 return {status: "success", message:"user deleted"}
+//         } else {
+//             return {status: "error", message: err.message}
+//         }
+//         }
+// }
 
-module.exports = Manager
+// module.exports = Manager
